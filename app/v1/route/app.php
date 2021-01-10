f7d046eb59acec9db86264815a68920cfe337305
sherk <sherkxuan@163.com>
2021-01-10 19:25:11 +0800

    路由改为动态

--- app/v1/route/app.php
+++ app/v1/route/app.php
@@ -9,11 +9,21 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 use think\facade\Route;
+use think\facade\Db;
 
-
+try {
+    $res = Db::name('api_info')->select();
+} catch (\think\db\exception\DbException $e) {
+    return '404 Not Found!';
+}
+//halt($res);
 //上传文件路由
-Route::rule('upload', 'Upload/upload02','post|get')
-    ->middleware(\app\middleware\ApiCount::class,'upload');
+foreach ($res as $v){
+    Route::rule($v['api_route'], $v['api_map'],'post|get')
+        ->middleware(\app\middleware\ApiCount::class,$v['api_route']);
+}
+/*Route::rule('upload', 'Upload/upload02','post|get')
+    ->middleware(\app\middleware\ApiCount::class,'upload');*/
 
 Route::miss(function() {
     return '404 Not Found!';
